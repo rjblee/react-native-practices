7942aeec79c8e107c5b84065ca2629f3ae3edfed
Ralph Lee <ralph.lee@live.ca>
2021-09-28 17:58:28 +0900

    Pass down imageScore props to child component

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -6,16 +6,19 @@ const ImageScreen = () => {
   return (
     <View>
       <ImageDetail
-        title="Is this working"
+        title="Forest"
         imageSource={require("../../assets/forest.jpg")}
+        imageScore={6}
       />
       <ImageDetail
         title="Beach"
         imageSource={require("../../assets/beach.jpg")}
+        imageScore={10}
       />
       <ImageDetail
         title="Mountain"
         imageSource={require("../../assets/mountain.jpg")}
+        imageScore={5}
       />
     </View>
   );
