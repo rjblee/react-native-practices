cf7d4fce2a0a6ed3dcfc79306a00acd6a1d22028
Ralph Lee <ralph.lee@live.ca>
2021-09-30 02:12:09 +0900

    Create randomRgb function

---
+++ rn-starter/src/screens/ColorScreen.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const ColorScreen = () => {
+  return (
+    <View>
+      <Button title="Add a colour" onPress={() => {}} />
+      <View
+        style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
+      ></View>
+    </View>
+  );
+};
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorScreen;
