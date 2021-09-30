51890e2a0def82bd07b775c8bd5364171b7c9911
Ralph Lee <ralph.lee@live.ca>
2021-09-30 22:51:03 +0900

    Add a FlatList to render a color box everytime button gets clicked

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+
   return (
     <View>
-      <Button title="Add a colour" onPress={() => {}} />
-      <View
-        style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
-      ></View>
+      <Button
+        title="Add a colour"
+        onPress={() => {
+          setColors([...colors, randomRgb()]);
+          console.log(colors);
+        }}
+      />
+      <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{ height: 100, width: 100, backgroundColor: item }}
+            ></View>
+          );
+        }}
+      ></FlatList>
     </View>
   );
 };
