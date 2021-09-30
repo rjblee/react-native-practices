2990fde9eff55d1d4d094fae9a641be8848d0fc4
Ralph Lee <ralph.lee@live.ca>
2021-09-30 22:51:47 +0900

    Add a Color Screen button to navigate to

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -3,14 +3,34 @@ import { Text, StyleSheet, View, Button } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import ComponentsScreen from "./ComponentsScreen";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   return (
     <View>
       <Text style={styles.text}>HomeScreen here</Text>
-      <Button title="Components" />
-      <TouchableOpacity>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => props.navigation.navigate("Components")}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => props.navigation.navigate("List")}
+      />
+      <Button
+        title="Go to Image Screen"
+        onPress={() => props.navigation.navigate("Image")}
+      />
+      <Button
+        title="Go to Counter Screen"
+        onPress={() => props.navigation.navigate("Counter")}
+      />
+      <Button
+        title="Go to Color Screen"
+        onPress={() => props.navigation.navigate("Color")}
+      />
+
+      {/* <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
         <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
 };
