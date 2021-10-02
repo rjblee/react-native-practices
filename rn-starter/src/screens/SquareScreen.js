66f1d90e1b2c43f6de54f4b23d6991ca4123da11
Ralph Lee <ralph.lee@live.ca>
2021-10-02 23:30:11 +0900

    Create a View tag with the color generator from the following RGB amount

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -1,13 +1,41 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
+const color_increment = 10;
+
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  console.log(green);
+
   return (
     <View>
-      <ColorCounter color="Red" />
-      <ColorCounter color="Green" />
-      <ColorCounter color="Blue" />
+      <ColorCounter
+        onIncrease={() => setRed(red + color_increment)}
+        onDecrease={() => setRed(red - color_increment)}
+        color="Red"
+      />
+      <ColorCounter
+        onIncrease={() => setGreen(green + color_increment)}
+        onDecrease={() => setGreen(green - color_increment)}
+        color="Green"
+      />
+      <ColorCounter
+        onIncrease={() => setBlue(blue + color_increment)}
+        onDecrease={() => setBlue(blue - color_increment)}
+        color="Blue"
+      />
+
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${green},${blue})`,
+        }}
+      ></View>
     </View>
   );
 };
